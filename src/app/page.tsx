0f823a74a06a52cb0dfacf0feb90041ff11a1e5c
Ayberk Uman <ayberkuman1@gmail.com>
2024-06-14 19:07:14 +0300

    feat: auth setup, schema with userId

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,35 @@
+import { auth } from "@/auth";
+import { SignInButton } from "@/components/SignInButton";
+import { SignOutButton } from "@/components/SignOutButton";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { database } from "@/db/database";
 import { jobs } from "@/db/schema";
+import { revalidatePath } from "next/cache";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+  const allJobs = await database.query.jobs.findMany();
+
   return (
     <main>
+      {session ? <SignOutButton /> : <SignInButton />}
       <form
         action={async (formData: FormData) => {
           "use server";
-          const apply = formData.get("apply");
-          await database.insert(jobs);
+          await database.insert(jobs).values({
+            name: formData.get("name") as string,
+            userId: session?.user?.id!,
+          });
+          revalidatePath("/");
         }}
       >
-        <input name="apply" type="text" placeholder="" />
-        <button type="submit">Submit</button>
+        <Input name="name" type="text" placeholder="Post a job" />
+        <Button type="submit">Post Job</Button>
       </form>
+      {allJobs.map((job) => (
+        <div key={job.id}>{job.name}</div>
+      ))}
     </main>
   );
 }
